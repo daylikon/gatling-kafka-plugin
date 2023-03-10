f7bea0ab31a72a602b497647e7334886523b5ce6
a.ugodnikov <a.ugodnikov@tinkoff.ru>
2023-03-10 17:00:53 +0300

    feat: kafka serdes only scala

--- src/main/scala/ru/tinkoff/gatling/kafka/request/KafkaSerdesImplicits.scala
+++ src/main/scala/ru/tinkoff/gatling/kafka/request/KafkaSerdesImplicits.scala
@@ -1,12 +1,16 @@
 package ru.tinkoff.gatling.kafka.request
 
+import com.typesafe.config.ConfigFactory
+import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient
+import io.confluent.kafka.serializers.{KafkaAvroDeserializer, KafkaAvroSerializer}
 import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde
 import org.apache.avro.generic.GenericRecord
-import org.apache.kafka.common.serialization.{Serde, Serdes => JSerdes}
+import org.apache.kafka.common.serialization.{Deserializer, Serde, Serializer, Serdes => JSerdes}
 import org.apache.kafka.streams.kstream.WindowedSerdes
 
 import java.nio.ByteBuffer
 import java.util.UUID
+import scala.jdk.CollectionConverters._
 
 trait KafkaSerdesImplicits {
   implicit def stringSerde: Serde[String]                             = JSerdes.String()
@@ -24,9 +28,22 @@ trait KafkaSerdesImplicits {
   implicit def intSerde: Serde[Int]                                   = JSerdes.Integer().asInstanceOf[Serde[Int]]
   implicit def javaIntegerSerde: Serde[java.lang.Integer]             = JSerdes.Integer()
   implicit def uuidSerde: Serde[UUID]                                 = JSerdes.UUID()
+  implicit val avroSerde: Serde[GenericRecord] = new GenericAvroSerde()
+
+  val schemaRegUrl: String =
+    ConfigFactory.parseResources(sys.props.getOrElse("gatling.conf.file", "gatling.conf"))
+    .getConfig("gatling")
+    .getString("schemaRegistryUrl")
+  implicit def classSerde[K]: Serde[K] = new Serde[K] {
+    override def serializer(): Serializer[K] = new KafkaAvroSerializer(
+      new CachedSchemaRegistryClient(schemaRegUrl.split(',').toList.asJava, 16),
+    ).asInstanceOf[Serializer[K]]
+
+    override def deserializer(): Deserializer[K] = new KafkaAvroDeserializer(
+      new CachedSchemaRegistryClient(schemaRegUrl.split(',').toList.asJava, 16),
+    ).asInstanceOf[Deserializer[K]]
+  }
 
   implicit def sessionWindowedSerde[T](implicit tSerde: Serde[T]): WindowedSerdes.SessionWindowedSerde[T] =
     new WindowedSerdes.SessionWindowedSerde[T](tSerde)
-
-  implicit val avroSerde: Serde[GenericRecord] = new GenericAvroSerde()
 }
